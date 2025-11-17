dbfdad479977dc605891d79c8ecce9b4bff68c0c
juanka0763 <juanka0763@gmail.com>
2025-11-16 21:59:42 -0500

    Cramos la clase Paciente, hereda atributos de la clase Persona

---
+++ src/main/java/Modelo/Paciente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author juank
+ */
+public class Paciente extends Persona {
+
+    private String telefono;
+
+    public Paciente(String nombre, String id, String telefono) {
+        super(nombre, id);
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String getTipoPersona() {
+        return "Paciente";   
+    }
+}
